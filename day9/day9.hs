4fdbccde24a61d2190daac4db4123a1934dbc890
Bart Platak <norfavrell@gmail.com>
2016-12-09 08:59:21 +0000

    Cleared up code for day9

--- day9/day9.hs
+++ day9/day9.hs
@@ -1,24 +1,30 @@
 import Data.List.Split (splitOn)
 
-test1 = "ADVENT"
-test2 = "A(1x5)BC"
-test3 = "(3x3)XYZ"
-test4 = "A(2x2)BCD(2x2)EFG"
-test5 = "(6x1)(1x3)A"
-test6 = "X(8x2)(3x3)ABCY"
-test7 = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
+-- Decompression type length
+data DecompressionType = V1 | V2 deriving Eq 
 
 -- Compute the decompressed length of a string 
-decompressedLength :: String -> Int 
-decompressedLength ('(':rest) = (cmd!!1)*decompressedLength repe + decompressedLength post
-    where cmd  = map read $ splitOn "x" $ takeWhile (/=')') rest 
-          repe = take (cmd!!0) $ drop 1 $ dropWhile (/=')') rest 
-          post = drop ((cmd!!0)+1) $ dropWhile (/=')') rest
-decompressedLength (a:rest) = 1 + decompressedLength rest
-decompressedLength []       = 0
+decompressedLength :: DecompressionType -> String -> Int 
+decompressedLength V1 ('(':rest) = c*l + (decompressedLength V1 postseq)
+    where (l,c,w) = parseMarker rest 
+          postseq = drop (l+w) rest 
+decompressedLength V2 ('(':rest) = c*(decompressedLength V2 subseq) + (decompressedLength V2 postseq)
+    where (l,c,w) = parseMarker rest 
+          postseq = drop (l+w) rest 
+          subseq  = take l $ drop w rest 
+decompressedLength v (a:rest)      = 1 + decompressedLength v rest
+decompressedLength _ []            = 0
+
+-- Parse the marker 
+parseMarker :: String -> (Int, Int, Int)
+parseMarker input = (l,c,length str + 1)
+    where str   = takeWhile (/=')') input 
+          [l,c] = map (read::String->Int) $ splitOn "x" str
 
 main = do
     strings <- lines <$> readFile "day9/data/data.in"
-    let solver1 = foldl (+) 0 . map decompressedLength
+    let solver1 = foldl (+) 0 . map (decompressedLength V1)
+    let solver2 = foldl (+) 0 . map (decompressedLength V2)
 
     print $ solver1 strings
+    print $ solver2 strings
