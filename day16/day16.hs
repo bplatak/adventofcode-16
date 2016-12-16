e1b1120f3b5743f9b15ee6687479acb72dc8f552
Bart Platak <norfavrell@gmail.com>
2016-12-16 14:09:04 +0000

    Added an infinite list solution to day16

--- day16/day16.hs
+++ day16/day16.hs
@@ -1,11 +1,18 @@
 import Data.List.Split (chunksOf)
 
--- Generate given amount of data using modified dragon curve and initial state
+-- -- Generate given amount of data using modified dragon curve and initial state
 modifiedDragonCurve :: [Bool] -> Int -> [Bool]
 modifiedDragonCurve s x = take x $ (iterate f s)!!idx
     where f s     = s ++ [False] ++ (map not . reverse) s 
           (a, x') = (fromIntegral $ length s, fromIntegral x)
           idx     = ceiling $ logBase 2 ((x'+1) / (a+1))
+
+-- An infinite list solution (http://stackoverflow.com/questions/41183330/infinite-self-referencing-list)
+-- This seems to require WAY too much memory, but it does feel a lot nicer.
+-- modifiedDragonCurve :: [Bool] -> [Bool]
+-- modifiedDragonCurve s = s ++ concatMap f [0..]
+--     where f n   = False : (map not . reverse) (take (len n) $ modifiedDragonCurve s)
+--           len n = (2^n)*(length s+1)-1
 
 -- Generate checksum 
 checksum :: [Bool] -> [Bool]
@@ -25,3 +32,4 @@ main = do
 
     print $ concat $ map (show . boolToInt) $ checksum $ modifiedDragonCurve part1 272
     print $ concat $ map (show . boolToInt) $ checksum $ modifiedDragonCurve part1 35651584
+
